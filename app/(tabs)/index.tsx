b3cf9119b8aaeb93955d368d1091ef89484d7cdc
Ryan <Ryan@MSI-RYAN>
2024-05-17 14:16:46 +0200

    update login page + code clean

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Image, Text, TouchableOpacity, Platform } from 'react-native';
-import { ThemedView } from '@/components/ThemedView'; // Correct import for ThemedView
-import { ThemedText } from '@/components/ThemedText'; // Correct import for ThemedText, if applicable
+import { TextInput, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // Handle login logic here
   };
 
   return (
     <ThemedView style={styles.container}>
       <Image
-        source={require('@/assets/images/logo.png')} // Your logo image
+        source={require('@/assets/images/logo.png')}
         style={styles.logo}
       />
       <ThemedText type="title" style={styles.title}>
-        Bienvenue !
+        Bienvenue sur Bumble B ! 🐝 
       </ThemedText>
       <TextInput
         style={styles.input}
@@ -40,7 +39,7 @@ export default function LoginScreen() {
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Connexion</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => {/* Handle forgot password logic */}}>
+      <TouchableOpacity onPress={() => {}}>
         <Text style={styles.forgotPassword}>Mot de passe oublié ?</Text>
       </TouchableOpacity>
     </ThemedView>
@@ -53,19 +52,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
-    backgroundColor: '#fff', // You can adjust this according to the theme
+    backgroundColor: '#fff',
   },
+
   logo: {
     width: 100,
     height: 100,
     marginBottom: 32,
   },
+
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 16,
-    color : "#1D3D47"
+    color: "#1f1f1f",
   },
+
   input: {
     width: '100%',
     padding: 16,
@@ -75,21 +77,25 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     backgroundColor: '#f9f9f9',
   },
+
   button: {
     width: '100%',
     padding: 16,
-    backgroundColor: '#1D3D47', // You can adjust this according to the theme
+    backgroundColor: '#ffbf00',
     borderRadius: 8,
     alignItems: 'center',
     marginVertical: 8,
   },
+
   buttonText: {
-    color: '#fff',
+    color: '#1f1f1f',
     fontWeight: 'bold',
     fontSize: 16,
   },
+
   forgotPassword: {
-    color: '#1D3D47', // You can adjust this according to the theme
+    color: '#1f1f1f',
     marginTop: 16,
   },
+  
 });
