a03a713a671cfb8b88118e644889d4a2ead47489
Nathan <nathan.pinard95@gmail.com>
2024-07-02 11:44:43 +0200

    splash screen + animation + redirection des routes

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,49 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, Image, StyleSheet, Animated } from 'react-native';
+import { useRouter } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const NotFoundSplashScreen: React.FC = () => {
+  const router = useRouter();
+  const opacity = useRef(new Animated.Value(1)).current;
 
-export default function NotFoundScreen() {
+  useEffect(() => {
+    const redirect = async () => {
+      await new Promise(resolve => setTimeout(resolve, 3000)); // Délai de 3 secondes
+
+      // Cache le splash screen natif avec une animation d'opacité
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 500, // Durée de l'animation en ms
+        useNativeDriver: true,
+      }).start(async () => {
+        await SplashScreen.hideAsync();
+        router.replace('/login'); // Redirection vers la page de connexion
+      });
+    };
+
+    redirect();
+  }, [opacity, router]);
+
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
+    <Animated.View style={[styles.container, { opacity }]}>
+      <Image source={require('../assets/images/splash.png')} style={styles.image} />
+    </Animated.View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    alignItems: 'center',
+    backgroundColor: '#FEF9C3',
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+  image: {
+    width: '80%',
+    height: '80%',
+    resizeMode: 'contain',
+  }
 });
+
+export default NotFoundSplashScreen;
